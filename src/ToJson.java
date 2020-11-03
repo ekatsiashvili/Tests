8126bcaf7a7c6200a04a6aad377df79134436b7a
Elena Katsiashvili <elena.katsiashvili@pragmaticplay.com>
2020-11-03 12:18:11 +0200

    Update

--- src/ToJson.java
+++ src/ToJson.java
@@ -1,10 +1,10 @@
+
 import java.lang.reflect.*;
 import java.util.Map;
 import java.util.Set;
 
 
 public class ToJson {
-
     private int result;
     private String error;
     private Map<String, Set<RemoteFile>> keywords;
@@ -15,30 +15,43 @@ public class ToJson {
         private String filePath;
         private long size = -1;
     }
-
     public static void main(String[] args) throws Exception {
 
-            Class<ToJson> toJsonClass = ToJson.class;
-            try {
-                Field result = toJsonClass.getDeclaredField("result");
-                System.out.println("{" + "\"" + result.getName() + "\":" + "\"" + result.getType() + "\"");
-                Field keywords = toJsonClass.getDeclaredField("keywords");
-                System.out.println(keywords.getName() + "\":" + "[{" + "\"" + keywords.getGenericType() + "\"");
-                System.out.println("[{");
+        Class<ToJson> toJsonClass = ToJson.class;
+
+        Object Json = Class.forName("ToJson").newInstance();
+        Class<?> cls = Json.getClass();
+        Field[] fields = cls.getDeclaredFields();
+           for (Field field : fields) {
+            Class<?> fld = field.getType();
+
+            if (fld.isPrimitive()) {
+                System.out.println("{\"" + field.getName() + "\":" + "\"" + fld.getName() + "\",");
+
+            }
+
+            if (fld.isInterface()) {
+                System.out.println("\"" + field.getName() + "\":" + "\"" + field.getGenericType() + "\"");
+
                 for (Class<?> class1 : ToJson.class.getDeclaredClasses()) {
                     for (Field field1 : class1.getDeclaredFields()) {
-                        System.out.println("\"" + field1.getName() + "\":" + "\"" + field1.getType() + "\"");
+
+                        System.out.println("\"" + field1.getName() + "\":" + "\"" + field1.getType() + "\",");
+
                     }
+                    Field error = toJsonClass.getDeclaredField("error");
+                    System.out.println("\"" + error.getName() + "\":" + "\"" + error.getType() + "\"" + "}");
                 }
-                System.out.println("]}");
-                System.out.println("]}");
-                Field error = toJsonClass.getDeclaredField("error");
-                System.out.println("\"" + error.getName() + "\":" + "\"" + error.getType() + "\"" + "}");
-            } catch (NoSuchFieldException e) {
-                e.printStackTrace();
             }
         }
     }
+}
+
+
+
+
+
+
 
 
 
